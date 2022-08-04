2c79273ba8101012c337ccd3e8f5bed0570bd18b
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2022-08-04 20:26:29 +0530

    Created sign up page layout UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,9 @@ const Navbar = () => {
         <li>
           <Link to="/profile">Profile</Link>
         </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
       </ul>
     </nav>
   )
---
+++ src/components/SignUp.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+const SignUp = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const collectData = (e) => {
+    e.preventDefault()
+
+    console.log({ name, email, password })
+    setName("")
+    setEmail("")
+    setPassword("")
+  }
+
+  return (
+    <form onSubmit={collectData} className="register">
+      <h1>Register</h1>
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="button" type="submit">
+        Sign Up
+      </button>
+    </form>
+  )
+}
+
+export default SignUp
