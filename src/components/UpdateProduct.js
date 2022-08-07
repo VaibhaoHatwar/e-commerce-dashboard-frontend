c560f2adf5dd5901194ea5237ee0a9fac75b0a44
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2022-08-07 20:52:50 +0530

    Integrated single product API to get prefill update form values

--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,11 +1,26 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const UpdateProduct = () => {
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
   const [category, setCategory] = useState("")
   const [company, setCompany] = useState("")
-  //   const [error, setError] = useState(false)
+
+  const params = useParams()
+
+  useEffect(() => {
+    getProductDetails()
+  }, [])
+
+  const getProductDetails = async () => {
+    let result = await fetch(`http://localhost:5000/product/${params.id}`)
+    result = await result.json()
+    setName(result.name)
+    setPrice(result.price)
+    setCategory(result.category)
+    setCompany(result.company)
+  }
 
   const updateProduct = (e) => {
     e.preventDefault()
