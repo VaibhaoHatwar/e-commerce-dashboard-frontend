3065c75285e6ece81aaa068dc1b1d313baa930b8
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2022-08-08 12:08:24 +0530

    Created Search bar and integrated Search API with it

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -24,9 +24,28 @@ const ProductList = () => {
     }
   }
 
+  const searchHandle = async (e) => {
+    let key = e.target.value
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`)
+      result = await result.json()
+      if (result) {
+        setProducts(result)
+      } else {
+        getProducts()
+      }
+    }
+  }
+
   return (
     <div className="product-list">
       <h1>Products List</h1>
+      <input
+        className="search-product-bar"
+        type="text"
+        placeholder="Search Product"
+        onChange={searchHandle}
+      />
       <ul>
         <li>S. No.</li>
         <li>Name</li>
@@ -34,20 +53,24 @@ const ProductList = () => {
         <li>Category</li>
         <li>Operation</li>
       </ul>
-      {products.map((item, index) => (
-        <ul key={item._id}>
-          <li>{index + 1}</li>
-          <li>{item.name}</li>
-          <li>{item.price}</li>
-          <li>{item.category}</li>
-          <li>
-            <button type="button" onClick={() => deleteProduct(item._id)}>
-              delete
-            </button>
-            <Link to={"/update/" + item._id}>Update</Link>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>
+              <button type="button" onClick={() => deleteProduct(item._id)}>
+                delete
+              </button>
+              <Link to={"/update/" + item._id}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Results Found</h1>
+      )}
     </div>
   )
 }
--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 const UpdateProduct = () => {
   const [name, setName] = useState("")
@@ -8,6 +8,7 @@ const UpdateProduct = () => {
   const [company, setCompany] = useState("")
 
   const params = useParams()
+  const navigate = useNavigate()
 
   useEffect(() => {
     getProductDetails()
@@ -22,10 +23,18 @@ const UpdateProduct = () => {
     setCompany(result.company)
   }
 
-  const updateProduct = (e) => {
+  const updateProduct = async (e) => {
     e.preventDefault()
 
-    console.log({ name, price, category, company })
+    let result = await fetch(`http://localhost:5000/product/${params.id}`, {
+      method: "Put",
+      body: JSON.stringify({ name, price, category, company }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    result = await result.json()
+    navigate("/")
   }
 
   return (
